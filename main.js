53aff352090b778a3313b9c1208a3e19ae2c51a6
Anthony Savant Moore <savant@frwdinc.com>
2018-02-21 14:41:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,10 +9,21 @@
 
 */
 
-'use strict';
-(function() {
-  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
-  var apiKey = "69e91067b3e5473760352dc1ba8a9ac2";
+  $(document).ready(function(){
+        $('#target').submit(function() {
+          var key = '69e91067b3e5473760352dc1ba8a9ac2';
+          var city = $('input#loc').val();
+          $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`, function(res) {
+            .append("html_str");
+            var html_str = "";
+            html_str += "<h1>" + res.name + ", " + res.sys.country + "</h1>";
+            html_str += "<h3>Temperature: " + res.main.temp +  " ℉</h3>";
+            html_str += "<h3>Weather: " + res.weather[0].description + "</h3>";
+            html_str += "<h4>High: " + res.main.temp_max + "</h4>";
+            html_str += "<h4>Low: " + res.main.temp_min + "</h4>";
+            $('div').html(html_str);
+          }, 'json');
 
-  });
-})();
+            return false;
+        })
+});
